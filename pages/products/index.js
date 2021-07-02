860de196b8d0745e64218a9d4dd7d2b741bea5e2
Sean OReilly <soreilly424@gmail.com>
2021-07-02 11:51:47 -0500

    removed bad api call

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,15 +1,14 @@
 import StoreHeading from '@/components/StoreHeading'
 import ProductListings from '@/components/ProductListings'
-import { getAllProductsInCollection, getProducts } from '@/lib/shopify'
-import { getAllArt, getLandingPage } from '../../lib/api'
+import { getAllProductsInCollection } from '@/lib/shopify'
+import { getAllArt } from '../../lib/api'
 import Layout from '@/components/Layout'
 
-function IndexPage({ products, art, landingPage, moreProducts }) {
+function IndexPage({ products, art }) {
   const photographs = art.filter((art) => art.category === 'photographs')
   const prints = art.filter((art) => art.category === 'prints')
-  const artPieces = landingPage[0].artPieces.map((art) => art.asset)
   return (
-    <Layout artPieces={artPieces} photographs={photographs} prints={prints}>
+    <Layout photographs={photographs} prints={prints}>
       <div className="mx-auto max-w-6xl">
         <StoreHeading />
         <ProductListings products={products} />
@@ -20,15 +19,11 @@ function IndexPage({ products, art, landingPage, moreProducts }) {
 
 export async function getStaticProps({ preview }) {
   const products = await getAllProductsInCollection()
-  const moreProducts = await getProducts()
   const art = await getAllArt(preview)
-  const landingPage = await getLandingPage(preview)
   return {
     props: {
       products,
       art,
-      landingPage,
-      moreProducts,
     },
   }
 }
