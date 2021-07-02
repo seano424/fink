3aa650f92428c17902fe5feca14f3aa502cbc536
Sean OReilly <soreilly424@gmail.com>
2021-07-02 09:30:05 -0500

    added bottom scroll removed size full

--- components/HorizontalImages.js
+++ components/HorizontalImages.js
@@ -13,10 +13,10 @@ export default function HorizontalImages({
   return (
     <main
       // Uncomment this if you want down scroll to also scroll right...
-      // onWheel={(e) =>
-      //   setDelta((e.currentTarget.scrollLeft += e.deltaY + e.deltaX))
-      // }
-      className="flex-1 overflow-y-hidden flex gap-x-10 md:h-screen pt-28 lg:pt-0"
+      onWheel={(e) =>
+        setDelta((e.currentTarget.scrollLeft += e.deltaY + e.deltaX))
+      }
+      className="flex-1 overflow-y-hidden flex gap-x-10 pt-28 lg:pt-0"
     >
       {images.map((image, idx) => (
         <div key={idx} className="min-w-max">
